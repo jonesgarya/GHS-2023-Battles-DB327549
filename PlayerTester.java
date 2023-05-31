ae15d3b8681b8bd3deda711fe0230a74fb889058
briod549 <135156955+briod549@users.noreply.github.com>
2023-05-31 11:23:40 -0700

    Create PlayerTester.java

---
+++ PlayerTester.java
@@ -0,0 +1,14 @@
+public class PlayerTester extends ConsoleProgram
+{
+    public void run()
+    {
+        // Test code here.
+        Player human = new Player();
+        
+        human.askShips();
+        
+       // human.printMyShips();
+        //human.printMyGuesses();
+    //    human.printOpponentGuesses();
+    }
+}
